6516b4e921dd6ef7685887ae1ea4d8da70dbdc3b
1+leo <91303905+macacotot@users.noreply.github.com>
2024-11-23 06:32:35 +1400

    Add files via upload

--- script.js
+++ script.js
@@ -7,12 +7,23 @@ const inputTransactionName = document.querySelector('#text')
 const inputTransactionAmount = document.querySelector('#amount')
 console.log({ inputTransactionName, inputTransactionAmount });
 
-const dummyTransactions = [
-  { id: 1, name: "Bolo de brigadeiro", amount: -20 },  //amount é o valor, que nesse caso significa despesa por estar negativo
-  { id: 2, name: "Salário", amount: 300 },
-  { id: 3, name: "Torta de limão", amount: -10 },
-  { id: 4, name: "Bateria", amount: 150 },
-];
+//let dummyTransactions = [
+  // { id: 1, name: "Bolo de brigadeiro", amount: -20 },  //amount é o valor, que nesse caso significa despesa por estar negativo
+  // { id: 2, name: "Salário", amount: 300 },
+  // { id: 3, name: "Torta de limão", amount: -10 },
+  // { id: 4, name: "Bateria", amount: 150 },
+//];
+
+const localStorageTransactions = JSON.parse(localStorage
+  .getItem('transactions'))
+
+  let transactions = localStorage
+.getItem('transactions') !== null ? localStorageTransactions : []
+
+const removeTransaction = ID => {
+  transactions = transactions.filter(transaction => transaction.id !== ID) // Vai excluir a transação que a pessoa eliminou clicando no X e vai retornar todas as outras.
+  init()
+}
 
 const addTransactionIntoDOM = (transaction) => {
   const operator = transaction.amount < 0 ? "-" : "+";  // é um IF ELSE reduzido sendo '?' o IF e o ':' o ELSE então se t.amount for menor se 0 vai ser '-' se não for será '+'
@@ -22,7 +33,7 @@ const addTransactionIntoDOM = (transaction) => {
 
   li.classList.add(CSSClass);
   li.innerHTML = `
-        ${transaction.name} <span> ${operator} R$ ${amountWithoutOperator}</span><button class="delete-btn">x</button>  `; //Serve para pegar a penas o valor absoluto, ou seja, sem os sinais (- ou +)
+        ${transaction.name} <span> ${operator} R$ ${amountWithoutOperator}</span><button class="delete-btn" onclick = "removeTransaction(${transaction.id})">x</button>  `;
 
   transactionUl.append(li);
   //   transactionUl.prepend(li);
@@ -37,21 +48,22 @@ const addTransactionIntoDOM = (transaction) => {
   }
 };
 
-// addTransactionIntoDOM(dummyTransactions[0]);
-// addTransactionIntoDOM(dummyTransactions[1]);
+// addTransactionIntoDOM(transactions[0]);
+// addTransactionIntoDOM(transactions[1]);
 
 const updateBalanceValues = () => {
-  const transactionsAmounts = dummyTransactions.map(
+  const transactionsAmounts = transactions.map(
     (transaction) => transaction.amount
   ); //O map nesse caso permite que manipulemos somente os atributos necessários para que realizemos o cálculo
 
+  console.log(transactionsAmounts);
 
   const income = transactionsAmounts //income são as receitas
     .filter((value) => value > 0) //filtra os valores do vetor, retornando somente os números que atendem a condição
     .reduce((accumulator, value) => accumulator + value, 0) // O reduce é uma alternativa mais otimizada para realizara a soma de valores 
     .toFixed(2);
 
-  const expense = Math.abs(transactionsAmounts  //Math.abs serve para 
+  const expense = Math.abs(transactionsAmounts  //Math.abs serve para pegar a penas o valor absoluto, ou seja, sem os sinais (- ou +)
     .filter((value) => value < 0)
     .reduce((accumulator, value) => accumulator + value, 0))
     .toFixed(2);
@@ -68,22 +80,41 @@ const updateBalanceValues = () => {
 
 
 const init = () => {
-  dummyTransactions.forEach(addTransactionIntoDOM);
+  transactionUl.innerHTML= ''
+  transactions.forEach(addTransactionIntoDOM);
   updateBalanceValues();
 };
 
 
 init();
+
+const upDateLocalStorage = () => {
+  localStorage.setItem('transactions', JSON.stringify(transactions));
+}
+
 const generateID = ()=> Math.round(Math.random()*1000) //Serve para criar os ID, que serve para identificação da transação de forma automatica
 
 form.addEventListener('submit', event => {
   event.preventDefault()
   const transName = inputTransactionName.value.trim()
   const transAmount = inputTransactionAmount.value.trim()
-  if (inputTransactionName.value.trim() === '' || inputTransactionAmount.value.trim() === '') { // o '||' é um 'OU' então nessa linha funciona como se o transName for vazio ou o valor, vai aparecer um alerta 
-    alert('Por gentileza preencha tanto o nome quanto o valor da transação!!!') 
-    return}
-  const transaction = { id: generateID, name: transName, amount: transAmount }
+  if(transName==='' || transAmount === ''){
+    alert('Por gentileza preencha tanto o nome quanto o valor da transação!!!')
+    return
+  }
+  const transaction = { id: generateID(), 
+    name: transName, 
+    amount: Number(transAmount) } //Somente o transAmount faz com que o código identifique o valor como string, não permitindo a operação matemática. -> A solução  
+  //console.log(transaction);
+
+  transactions.push(transaction)
+
+  init()
+
+  upDateLocalStorage()
+  
+  inputTransactionName.value = ''
+  inputTransactionAmount.value = ''
 })
 
 
